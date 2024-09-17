bdfe98be9b39f7ac66d81afdca6662e2a2407720
Groothoff <bho.groothoff@btc-ag.com>
2024-09-17 15:35:14 +0200

    ArithmetischeOperationenAdd

---
+++ src/main/java/org/example/ArithmetischeOperationen.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class ArithmetischeOperationen {
+    public static void main(String[] args) {
+
+        long zahl = 0;
+
+        System.out.println(++zahl);
+        System.out.println(zahl);
+        System.out.println((zahl++));
+        System.out.println(zahl);
+
+
+
+
+
+
+
+        }
+    }
+
