a95ba0f6a9f30ab9bc3f88bde0d977d371169ed3
Groothoff <bho.groothoff@btc-ag.com>
2024-09-18 13:05:29 +0200

    divisionscheck fertig

---
+++ src/main/java/org/example/Divisionscheck.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Divisionscheck {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int divider;
+        int divBisHier;
+        int counter = 0;
+        do {
+            System.out.println("Gib eine Zahl an, die als Divisor benutzt werden soll");
+            divider = scanner.nextInt();
+        }while(divider<=1);
+
+
+        do {
+            System.out.println("Gib eine Zahl an, bis zu der die Teilbarkeit überprüft werden soll.");
+            divBisHier = scanner.nextInt();
+        }while(divBisHier<=1);
+
+
+        while(counter<=divBisHier){
+        if(counter%divider==0){
+            System.out.println("Die Zahl "+ counter+ " ist durch "+ divider+" teilbar.");
+        }
+        counter++;
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+    }
