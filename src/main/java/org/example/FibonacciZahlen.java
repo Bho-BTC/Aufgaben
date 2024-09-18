3e84d06813ebb6062e1ce2f8de2ab9925ef3eb50
Groothoff <bho.groothoff@btc-ag.com>
2024-09-18 16:02:19 +0200

    FibonacciZahlen fertig

---
+++ src/main/java/org/example/FibonacciZahlen.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+public class FibonacciZahlen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int firstNbr = 0;
+        int secondNbr = 1;
+
+        System.out.println("Gib eine Zahl ein.");
+        int maxRounds = scanner.nextInt();
+
+        int ergebnis = 0;
+        while(ergebnis<=maxRounds){
+            ergebnis = firstNbr+secondNbr;
+            firstNbr = secondNbr;
+            secondNbr=ergebnis;
+
+            if(ergebnis<maxRounds) {
+                System.out.println(ergebnis);
+            }
+
+        }
+    }
+}
