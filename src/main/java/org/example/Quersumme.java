e53f42c1c593e180637c87de64d07fbfd605dd02
Groothoff <bho.groothoff@btc-ag.com>
2024-09-18 09:34:10 +0200

    quersumme fertig

--- src/main/java/org/example/Quersumme.java
+++ src/main/java/org/example/Quersumme.java
@@ -10,7 +10,22 @@ public class Quersumme {
         System.out.println("Geben sie eine Zahl ");
 
         long Zahl = scanner.nextLong();
+        long ergebnis = 0;
 
+        String zahlString = Zahl +"";
+
+        int length = zahlString.length();
+
+        while (length > 0) {
+            String stellenwert = String.valueOf(zahlString.charAt(length - 1));
+            ergebnis += Long.parseLong(stellenwert);
+
+            length--;
+        }
+
+        System.out.println("Die Quersumme ist: "+ ergebnis);
+
+        }
 
 
 
@@ -18,4 +33,4 @@ public class Quersumme {
     }
 
 
-}
+
