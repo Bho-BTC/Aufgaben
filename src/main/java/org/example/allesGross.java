21a08e0532176f97dceb512bda6c30671eb5bd9e
Groothoff <bho.groothoff@btc-ag.com>
2024-09-19 13:45:15 +0200

    to uppercase fertig

---
+++ src/main/java/org/example/allesGross.java
@@ -0,0 +1,32 @@
+package org.example;
+import java.util.Scanner;
+public class allesGross {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        String output = "";
+
+        char reconversion;
+        int length = input.length();
+        int counter =0;
+
+        while (counter<length) {
+            char currChar = input.charAt(counter);
+             int ascii = (int)currChar;
+             while(ascii>90){
+                 ascii-=32;
+             }
+             reconversion = (char)ascii;
+             output = output + reconversion;
+             counter++;
+        }
+
+        System.out.println(output);
+
+
+
+    }
+}
